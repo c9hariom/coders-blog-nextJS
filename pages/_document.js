19f4bc41f9ced3c7a8de6fc5282dff6adce71b93
Hariom Singh <22169420+c9hariom@users.noreply.github.com>
2024-03-02 13:35:22 +0530

    added bootstrap

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document () {
   return (
@@ -6,8 +7,18 @@ export default function Document () {
       <Head>
         <title>Your Page Title</title>
         <meta name='description' content='Your page description' />
+        <link
+          href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
+          rel='stylesheet'
+          integrity='sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH'
+          crossorigin='anonymous'
+        />
       </Head>
       <body>
+        <Script
+          src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz'
+          strategy='lazyOnload'
+        ></Script>
         <Main />
         <NextScript />
       </body>
