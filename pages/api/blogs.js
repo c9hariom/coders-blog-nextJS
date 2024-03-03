25bd2453f692a344e9fe3cb057b30cfe31df30f9
Hariom Singh <22169420+c9hariom@users.noreply.github.com>
2024-03-03 20:51:12 +0530

    blogs api regined

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -3,6 +3,15 @@ import { promises as fs } from 'fs';
 
 export default async function handler(req, res) {
     const files = await fs.readdir('blogposts');
-    res.status(200).json(files)
+    let data = [];
+    for(let i=0; i< files.length ;i++){
+        let data2 = await fs.readFile('blogposts/'+files[i],'utf-8');
+        data2 = JSON.parse(data2)
+        let temp ={}
+        temp.title = data2.title
+        temp.content = data2.content
+        data.push(temp);
+    }
+    res.status(200).json(data)
   }
   
--- pages/api/getBlog.js
+++ pages/api/getBlog.js
@@ -1,11 +1,13 @@
 import { promises as fs } from 'fs'
 
 export default async function handler (req, res) {
-  try{const slug = req.query.blog
-  const files =await fs.readFile(`blogposts/${slug}.json`, "utf-8")
-  if(files){
-    res.status(200).json(JSON.parse(files))
-  }} catch {
-    res.status(404).json({status:"not found"})
+  try {
+    const slug = req.query.blog
+    const files = await fs.readFile(`blogposts/${slug}.json`, 'utf-8')
+    if (files) {
+      res.status(200).json(JSON.parse(files))
+    }
+  } catch {
+    res.status(404).json({ status: 'not found' })
   }
 }
