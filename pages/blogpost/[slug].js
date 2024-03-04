cfcdda9858ad3b6780294cc393c7e4c38070b97f
Hariom Singh <22169420+c9hariom@users.noreply.github.com>
2024-03-04 18:03:06 +0530

    added server side rendering for blog post and added paras

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useRouter } from 'next/router'
 import styles from '../../styles/blogPost.module.css'
 
-const Slug = () => {
+const Slug = props => {
   const router = useRouter()
 
-  const [blog, setBlog] = useState(null)
+  const blog = props.data
+  let para = []
 
-  const slug = router.query.slug
+  // const slug = router.query.slug
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (slug) {
-        const response = await fetch(
-          'http://localhost:3000/api/getBlog?blog=' + slug
-        )
-        const data = await response.json()
-        setBlog(data)
-      }
-    }
-    fetchData()
-  }, [slug])
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     if (slug) {
+  //       const response = await fetch(
+  //         'http://localhost:3000/api/getBlog?blog=' + slug
+  //       )
+  //       const data = await response.json()
+  //       setBlog(data)
+  //     }
+  //   }
+  //   fetchData()
+  // }, [slug])
 
   return (
     <div className={`container ${styles.main}`}>
@@ -30,7 +31,12 @@ const Slug = () => {
             <h3>{blog.title}</h3>
           </div>
           <hr />
-          <p>{blog.content}</p>
+          {(() => {
+            para = blog.content.split('\n\n')
+          })()}
+          {para.map((data, index) => {
+            return <p key={index}>{data}</p>
+          })}
         </div>
       )}
     </div>
@@ -38,3 +44,13 @@ const Slug = () => {
 }
 
 export default Slug
+
+export async function getServerSideProps (context) {
+  if (context.query.slug) {
+    const response = await fetch(
+      'http://localhost:3000/api/getBlog?blog=' + context.query.slug
+    )
+    const data = await response.json()
+    return { props: { data } }
+  }
+}
