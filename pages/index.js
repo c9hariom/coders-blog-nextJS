f6b1d10e2ef0c060c17e508e65d3a7eda1732629
Hariom Singh <22169420+c9hariom@users.noreply.github.com>
2024-03-05 20:50:26 +0530

    dangerouslySetinnerHTML added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import { Inter } from 'next/font/google'
-import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home () {
+  function createMarkup () {
+    return {
+      __html:
+        "<center><h3>Coders-Nexus : A one stop solution for a coder's need</h3></center>"
+    }
+  }
+
   return (
-    <div></div>
+    <div>
+      <div dangerouslySetInnerHTML={createMarkup()} />
+    </div>
   )
 }
