3bc5eadda7e11adcf61eea22f9e9046196cb4896
Hariom Singh <22169420+c9hariom@users.noreply.github.com>
2024-03-06 18:59:06 +0530

    refined blogs api for page and count to habdle infinite scroll

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -2,9 +2,12 @@ import { promises as fs } from 'fs';
 
 
 export default async function handler(req, res) {
+    const count = req.query.count;
+    const page = req.query.page;
     const files = await fs.readdir('blogposts');
+    const totalResults = files.length;
     let data = [];
-    for(let i=0; i< files.length ;i++){
+    for(let i=((page-1)*count); i< page*count ;i++){
         let data2 = await fs.readFile('blogposts/'+files[i],'utf-8');
         data2 = JSON.parse(data2)
         let temp ={}
@@ -13,6 +16,7 @@ export default async function handler(req, res) {
         temp.content = data2.content
         data.push(temp);
     }
-    res.status(200).json(data)
+
+    res.status(200).json({totalResults,data})
   }
   
